658daed6a065589de9eba8e4e5ac1bba947cc955
amar9elbeg <89896446+amar9elbeg@users.noreply.github.com>
2021-10-05 13:14:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+
+/* Step 1: Create a higher-order function
+ * Create a higher-order function named consume with 3 parameters: a, b and cb
+ * The first two parameters can take any argument (we can pass any value as argument)
+ * The last parameter accepts a callback
+ * The consume function should return the invocation of cb, passing a and b into cb as arguments
+ */
+function consume(a, b, cb) {
+  return cb(a, b);
+}
+
+/* Step 2: Create several functions to callback with consume();
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num) {}
+console.log(summation(4));
